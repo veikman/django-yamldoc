0468413988ba94c0a59804deea227c6a88de8548
Viktor Eikman <viktor.eikman@gmail.com>
2021-08-12 20:12:06 +0200

    Test task.

--- tasks.py
+++ tasks.py
@@ -10,14 +10,24 @@ from invoke import task
 @task()
 def clean(c):
     """Destroy prior artifacts."""
-    c.run('rm dist/*', warn=True)
-    c.run('rmdir dist', warn=True)
+    c.run('rm *.whl *.tar.gz dist/*', warn=True)
+    c.run('rmdir build dist src/*.egg-info', warn=True)
+    c.run(r'find . | grep -E "(__pycache__|\.pyc|\.pyo$$)" | xargs rm -rf')
 
 
 @task(pre=[clean])
 def build(c):
     """Build wheel in dist/."""
     c.run('python -m build')
+
+
+@task(pre=[build])
+def test(c):
+    """Build, install, and then run unit tests."""
+    with c.cd('dist'):
+        c.run('sudo pip install --force-reinstall *.whl')
+    with c.cd('src'):
+        c.run('python runtests.py')
 
 
 @task()
